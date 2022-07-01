815e53ec3ea9bc9e6cc53dbde875533174a863bf
Diyor End <diyorend@gmail.com>
2022-07-01 22:06:19 +0500

    session 1 and 2

--- index2.js
+++ index2.js
@@ -95,17 +95,15 @@ function formatStr(str) {
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
 function letterChanges(str) {
-  let newStr = str.toLowerCase().replace(/[a-z]/gi, (char) => {
-    if (char === "z" || char === "Z") {
-      return "a";
-    } else {
-      return String.fromCharCode(char.charCodeAt() + 1);
-    }
-  });
-
-  newStr = newStr.replace(/a|e|i|o|u/gi, (vowel) => vowel.toUpperCase());
-
-  return newStr;
+  // let newStr = str.toLowerCase().replace(/[a-z]/gi, (char) => {
+  //   if (char === "z" || char === "Z") {
+  //     return "a";
+  //   } else {
+  //     return String.fromCharCode(char.charCodeAt() + 1);
+  //   }
+  // });
+  // newStr = newStr.replace(/a|e|i|o|u/gi, (vowel) => vowel.toUpperCase());
+  // return newStr;
 }
 
 // Call Function
